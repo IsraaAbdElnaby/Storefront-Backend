8f3c4341897c77c3f0700464f7bce8cf24f84519
IsraaAbdElnaby <israa.tobgy@gmail.com>
2023-01-25 10:12:30 +0200

    Add Product API Endpoints

--- src/models/product.ts
+++ src/models/product.ts
@@ -18,4 +18,29 @@ export class ProductEntity {
             throw new Error(`Something went wrong! cannot retrieve products ${err}`);
         }
     }
+
+    async show(id: string): Promise<Product> {
+        try {
+            const con = await pool.connect(); //connect to the DB
+            const qry = "SELECT * FROM products WHERE id=($1)";
+            const result = await con.query(qry, [id]);  //stores the resulting rows from the DB query
+            con.release();
+            return result.rows[0];
+        } catch (err){
+            throw new Error(`Something went wrong! couldn't find product ${id}. ${(err as Error).message}`);
+        }
+    }
+
+    async create(p: Product): Promise<Product> {
+        try {
+            const con = await pool.connect(); //connect to the DB
+            const qry = "INSERT INTO products(name, price) VALUES ($1, $2) RETURNING *";
+            const result = await con.query(qry, [p.name, p.price]);  //stores the resulting rows from the DB query
+            con.release();
+            return result.rows[0];
+        } catch (err){
+            throw new Error(`Couldn't add new product ${p.name}. ${(err as Error).message}`);
+        }
+    }
+
 } 
