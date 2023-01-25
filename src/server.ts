41ed7539764adfe22a0715d9c46d7b3c7aad6895
IsraaAbdElnaby <israa.tobgy@gmail.com>
2023-01-25 17:01:48 +0200

    Add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors';
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
+var corsOptions = {
+    origin: 'http://storefront.com',
+    optionsSuccessStatus: 200 
+}
+
+app.use(cors(corsOptions));
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for only example.com.'})
 })
+
+
+
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
